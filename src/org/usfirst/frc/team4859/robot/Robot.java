af8e071e1ab9847c5d92e7c91dafcafe25f08e34
byronrobotics <byronrobotics@gmail.com>
2015-12-19 14:34:19 -0600

    jjj

--- src/org/usfirst/frc/team4859/robot/Robot.java
+++ src/org/usfirst/frc/team4859/robot/Robot.java
@@ -7,7 +7,7 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 
 public class Robot extends IterativeRobot {
 	//Create subsystems
