d73982c6d2a8ae86144cbb7cceddf1808820b442
byronrobotics <byronrobotics@gmail.com>
2017-01-07 19:09:37 -0600

    Fixed CAN Talon libraries/importing

--- src/org/usfirst/frc/team4859/robot/commands/DriveWithJoystick.java
+++ src/org/usfirst/frc/team4859/robot/commands/DriveWithJoystick.java
@@ -1,7 +1,6 @@
 package org.usfirst.frc.team4859.robot.commands;
 
 import org.usfirst.frc.team4859.robot.Robot;
-
 import edu.wpi.first.wpilibj.command.Command;
 
 public class DriveWithJoystick extends Command {
