7bce8e26005965e660ac5f1f0d87274bf798f5bc
Andy H <fan.horror@gmail.com>
2017-02-11 15:06:37 -0600

    kool light stuff

--- src/org/usfirst/frc/team4859/robot/subsystems/Chassis.java
+++ src/org/usfirst/frc/team4859/robot/subsystems/Chassis.java
@@ -5,6 +5,8 @@ import org.usfirst.frc.team4859.robot.RobotMap;
 import org.usfirst.frc.team4859.robot.ThrottleLookup.ThrottleLookup;
 import org.usfirst.frc.team4859.robot.commands.DriveWithJoystick;
 import com.ctre.CANTalon;
+
+import edu.wpi.first.wpilibj.DigitalOutput;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -17,6 +19,8 @@ public class Chassis extends Subsystem {
 	
 	public static CANTalon motorChassisBackLeft = new CANTalon(RobotMap.talonIDChassisBackLeft);
 	public static CANTalon motorChassisBackRight = new CANTalon(RobotMap.talonIDChassisBackRight);
+	
+	public static DigitalOutput lightStrip = new DigitalOutput(0);
 
 	// Creates robot drive configuration with four motors
 	public static RobotDrive chassisDrive = new RobotDrive(motorChassisFrontLeft, motorChassisBackLeft, motorChassisFrontRight, motorChassisBackRight);
